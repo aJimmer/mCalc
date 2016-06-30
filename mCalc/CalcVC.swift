642e42be99052f4395a5e4fa6488b294f62fc0d3
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2016-06-29 17:44:42 -0700

    first commit

--- mCalc/CalcVC.swift
+++ mCalc/CalcVC.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 class CalcVC: UIViewController {
+    
+    // reference -> + − × √ % 
+    
+    @IBOutlet weak var display: UILabel!
+    var displayValue : Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = "\(newValue)"
+        }
+    }
+    
+    var userIsTypingNumber = false
+    var numberOne : Double = 0
+    var numberTwo : Double = 0
+    var currentOperator : String = ""
+    let defaults = NSUserDefaults.standardUserDefaults()
+    var savedNumber : Double = 0
+    var memorType : String = ""
+    var tax : Double = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +42,62 @@ class CalcVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func digitTapped(sender: UIButton) {
+        
+        
+        
     }
-    */
-
+    
+    @IBAction func operatorTapped(sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func equalTapped(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func minusTenPerc(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func plusTax(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func memoryTapped(sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func clearTapped(sender: AnyObject) {
+        
+        
+        
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
